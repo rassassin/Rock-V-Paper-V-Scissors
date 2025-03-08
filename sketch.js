1fc29abe4f1a9a0b166ee32639c47466b9d2fb50
Reece Thandi <reecethandi11@gmail.com>
2025-03-08 17:42:55 +0000

    added entity counts and made them match colour of their entity

--- sketch.js
+++ sketch.js
@@ -20,8 +20,15 @@ function draw() {
   qt.clear();
   background(220);
 
+  let rocks = 0;
+  let papers = 0;
+  let scissors = 0;
+
   for (const entity of entities) {
     qt.add(entity);
+    if (entity.type == "rock") rocks++;
+    if (entity.type == "paper") papers++;
+    if (entity.type == "scissors") scissors++;
     if (allSame) {
       if (!lastType) {
         lastType = entity.type;
@@ -30,7 +37,7 @@ function draw() {
       if (lastType != entity.type) allSame = false;
     }
   }
-
+  showHud(rocks, papers, scissors);
   qt.purge();
 
   if (allSame && !resetTimeout) {
@@ -48,6 +55,25 @@ function draw() {
   }
 
   qt.show();
+}
+
+function showHud(rocks, papers, scissors) {
+  const leftPadding = 50;
+  const topPadding = 50;
+  const gap = 50;
+  const strings = [`Rocks: ${rocks}`, `Paper: ${papers}`, `Scissors: ${scissors},`, `Total: ${n}`];
+  noStroke();
+  textSize(16);
+  let position = leftPadding;
+  for (let i = 0; i < strings.length; i++) {
+    if (i === 0) fill("red");
+    if (i === 1) fill("purple");
+    if (i === 2) fill("green");
+    if (i > 2) fill("black");
+    text(strings[i], position, topPadding);
+    position += textWidth(strings[i]);
+    position += gap;
+  }
 }
 
 const getRandomInt = (max, min = 0) => Math.floor(Math.random() * (max - min + 1)) + min;
